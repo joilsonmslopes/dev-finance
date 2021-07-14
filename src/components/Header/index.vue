c292663d2b043b3a8b15890fb11cfa36cb76ae10
Joilson Miranda <joilsonmirandasl@hotmail.com>
2021-07-14 17:53:21 -0300

    chore: add header and initial transactions

---
+++ src/components/Header/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <div class="logo">
+            <img :src="logoImg" alt="dev.finance">
+        </div>
+    </header>
+</template>
+
+<script>
+import logoImg from '../../assets/images/logo.svg';
+
+export default {
+    name: 'Header',
+    data() {
+        return {
+            logoImg
+        }
+    }
+}
+</script>
+
+<style src="./style.scss" lang="scss" scoped />
---
+++ src/components/Transactions/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <section>
+        <div class="transactions">
+            <div class="transaction">
+                <div class="title">
+                    <h1>Entradas</h1>
+                    <img :src="incomesImg" alt="Entradas">
+                </div>
+                <h2>
+                    R$ 17.400,00
+                </h2>
+            </div>
+            <div class="transaction">
+                <div class="title">
+                    <h1>Saídas</h1>
+                    <img :src="expenseImg" alt="Saídas">
+                </div>
+                <h2>
+                    R$ 1.259,00
+                </h2>
+            </div>
+            <div class="transaction">
+                <div class="title">
+                    <h1>Total</h1>
+                    <img :src="totalImg" alt="Total">
+                </div>
+                <h2>
+                    R$ 16.141,00
+                </h2>
+            </div>
+        </div>
+
+        <div class="newTransaction">
+            <button class="btn">Adicionar Transação <img :src="newTransaction" alt="Nova Transação"></button>
+        </div>
+
+        
+    </section>
+</template>
+
+<script>
+import incomesImg from '../../assets/images/incomes.svg';
+import expenseImg from '../../assets/images/expense.svg';
+import totalImg from '../../assets/images/total.svg';
+import newTransaction from '../../assets/images/plus.svg';
+
+export default {
+    name: 'Transactions',
+    data() {
+        return {
+            incomesImg,
+            expenseImg,
+            totalImg,
+            newTransaction
+        }
+    }
+}
+</script>
+
+<style src="./style.scss" lang="scss" scoped />
